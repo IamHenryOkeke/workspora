5b4889f5c9ab8417bd50268d8f559c87ed133bdd
Okeke Nmesomachukwu Henry <iamhenryokeke@gmail.com>
2026-03-03 19:45:33 +0100

    chore: project init

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from "@/components/auth/login-form";
+
+export default function Login() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <LoginForm />
+    </main>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-accent">Hello, World!</h1>
+    </div>
+  );
+}
